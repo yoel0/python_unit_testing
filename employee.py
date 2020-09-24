ad940b663b4365ec6bfcb61d1f5457c524948326
yoel <yoel@macbook-pro.home>
2020-09-24 14:11:06 -0700

    [employee] Add class and instance

---
+++ employee.py
@@ -0,0 +1,25 @@
+class Employee:
+  """A sample Employee class"""
+
+  raise_amount = 1.15
+
+  def __init__(self, first, last, pay):
+    self.first = first
+    self.last = last
+    self.pay = pay
+
+  @property
+  def email(self):
+    return '{}.{}@sei713.com'.format(self.first, self.last).lower()
+
+  @property
+  def fullname(self):
+    return '{} {}'.format(self.first, self.last)
+
+  def apply_raise(self):
+    self.pay = int(self.pay * self.raise_amount)
+
+bell = Employee('Bell', 'Cranel', '1000000')
+
+# print(bell.email)
+# print(bell.fullname)
